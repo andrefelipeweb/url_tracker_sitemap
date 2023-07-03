bbf94901b80f45fbd335d0c1dd79bf10240eb3b6
André Felipe Vale da Silva <andre.silva@a2bi.com.br>
2023-07-03 14:03:09 +0000

    mapeador.js edited online with Bitbucket

--- mapeador.js
+++ mapeador.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3334;
+const port = 22;
 
 const isValidURL = (url) => {
   const regex = /^https?:\/\/\w+(\.\w+)*\/([\w-]+\/)+$/;
